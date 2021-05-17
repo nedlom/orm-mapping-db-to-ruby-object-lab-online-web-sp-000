e7355169eb0cc3c8f9a2a72709abd4f74588fe84
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-17 18:56:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,13 +106,14 @@ class Student
       SQL
       
     DB[:conn].execute(sql, x).map do |row|
+      binding.pry
       self.new_from_db(row)
     end
   end
     
-    def self.first_student_in_grade_10
-      self.first_X_students_in_grade_10(1)
-    end
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
   
   def self.all_students_in_grade_X(x)
     sql = <<-SQL

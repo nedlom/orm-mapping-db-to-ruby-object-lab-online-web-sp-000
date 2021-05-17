62f457db19251639817871970ef0bff0f2dcc088
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-17 18:57:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,7 +111,7 @@ class Student
   end
     
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    self.first_X_students_in_grade_10(1)[0]
   end
   
   def self.all_students_in_grade_X(x)

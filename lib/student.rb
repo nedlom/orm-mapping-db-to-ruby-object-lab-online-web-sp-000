6c5ad3067ceae63db0814dcc00fb99df159085cb
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-17 18:36:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,16 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
+  
+  def self.find_by_name(name)
+    sql =<<-
+  end
 end

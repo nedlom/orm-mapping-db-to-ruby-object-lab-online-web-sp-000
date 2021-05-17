6a8013b20b65396b23fa04e373033de91f3ebb80
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-17 18:46:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,14 +94,15 @@ class Student
       self.new_from_db(row)
     end
     
-    def self.all_students_in_grade_9
+    def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * 
       FROM students
-      WITH grade = 9
+      WITH grade = 10
+      LIMIT ?
       SQL
       
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end

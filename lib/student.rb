d40fc658bdfc11204d313983273a6759fd5cb4d4
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-17 18:47:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,5 +105,21 @@ class Student
     DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
+    
+    def self.first_student_in_grade_10
+      self.first_X_students_in_grade_10(1)
+    end
+  end
+    
+  def self.all_students_in_grade_X
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WITH grade = ?
+      SQL
+      
+    DB[:conn].execute(sql, ?).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
